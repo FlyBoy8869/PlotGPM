a840d4900c15a8768521a0315e1e3614fdf89dc7
Charles Cognato <charlescognato@gmail.com>
2026-06-13 13:31:55 -0400

    refactor: Address code analysis complaint.

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import configparser
 import os
 
-app_root = os.path.dirname(__file__)
+app_root: str = os.path.dirname(__file__)
 CONFIG_FILE = os.path.join(app_root, "config.ini")
 
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
 config.add_section("appInfo")
-config["appInfo"]["appRoot"] = os.path.dirname(__file__)
+config["appInfo"]["appRoot"] = app_root
 config["appInfo"]["appIcon"] = os.path.join(config['appInfo']['appRoot'], "icon.ico")
